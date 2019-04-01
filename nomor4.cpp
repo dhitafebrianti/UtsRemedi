6c14081e55d244f682949908f624698270fa0e13
dhitafebrianti <49123000+dhitafebrianti@users.noreply.github.com>
2019-04-01 14:08:00 +0800

    Create nomor4.cpp

---
+++ nomor4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int bil;
+cout<<"masukkan nilai:";
+cin>>bil;
+if (bil==80){
+cout<<"nilai"<<bil<<"=A";
+} else if (bil==50) {
+cout<<"nilai"<<bil<<"=b";
+} else if (bil==10) {
+cout<<"nilai"<<bil<<"=c";
+} else if (bil==0) {
+cout<<"nilai"<<bil<<"=d";
+}
+return 0;
+}
